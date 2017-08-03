d400848d41287ced80df0bde38011370b965e9fb
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2017-08-03 10:48:05 +0530

    Adding linear contrast stretching

---
+++ assign1/2/code/myLinearContrastStretching.m
@@ -0,0 +1,11 @@
+function output = myLinearContrastStretching(img)
+    img = double(img);
+    maximum = max(img(:));
+    minimum = min(img(:));
+    [h, w] = size(img);
+    for i = 1:h
+        for j = 1:w
+            output(i, j) = (img(i, j) - minimum) * 256.0 / (maximum - minimum);
+        end
+    end
+end
--- assign1/2/code/myMainScript.m
+++ assign1/2/code/myMainScript.m
@@ -2,5 +2,32 @@
 
 tic;
 %% Your code here
+%% Setup
 
-toc;
+%% Linear constrast stretching
+input = imread('../data/barbara.png');
+output = myLinearContrastStretching(input);
+% Displaying images, press any key to continue
+imshow(input, 'InitialMagnification', 'fit')
+axis on, colorbar, waitforbuttonpress
+imshow(uint8(output), 'InitialMagnification', 'fit')
+axis on,colorbar, waitforbuttonpress
+
+input = imread('../data/TEM.png');
+output = myLinearContrastStretching(input);
+% Displaying images, press any key to continue
+imshow(input, 'InitialMagnification', 'fit')
+axis on, colorbar, waitforbuttonpress
+imshow(uint8(output), 'InitialMagnification', 'fit')
+axis on,colorbar, waitforbuttonpress
+
+input = imread('../data/canyon.png');
+output = zeros(size(input));
+for i=1:3
+    output(:, :, i) = myLinearContrastStretching(input(:, :, i));
+end
+% Displaying images, press any key to continue
+imshow(input, 'InitialMagnification', 'fit')
+axis on, colorbar, waitforbuttonpress
+imshow(uint8(output), 'InitialMagnification', 'fit')
+axis on,colorbar, waitforbuttonpress
