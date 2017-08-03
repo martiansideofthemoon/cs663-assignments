a5184abbd866da3fc6052152461febc6e8d1f128
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2017-08-03 05:49:13 +0530

    Adding display script

--- assign1/1/code/myMainScript.m
+++ assign1/1/code/myMainScript.m
@@ -3,4 +3,18 @@
 tic;
 %% Your code here
 
-toc;
+%% Shrinking images
+input = imread('../data/circles_concentric.png');
+output_two = myShrinkImageByFactorD(input, 2);
+output_three = myShrinkImageByFactorD(input, 3);
+% Displaying images, press any key to continue
+imshow(input, 'InitialMagnification','fit')
+axis on, colorbar, waitforbuttonpress
+imshow(output_two, 'InitialMagnification','fit')
+axis on,colorbar, waitforbuttonpress
+imshow(output_three, 'InitialMagnification','fit')
+axis on, colorbar
+
+%% Bilinear Interpolation
+
+%% Nearest Neighbours
