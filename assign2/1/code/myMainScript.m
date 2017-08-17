4fcfdf82054b33bf856df6742a8061cbe7ccb7d3
Mohit Madan <mohitmadan71@gmail.com>
2017-08-17 17:46:13 +0530

    myMainScript.m updated to run unsharp masking

--- assign2/1/code/myMainScript.m
+++ assign2/1/code/myMainScript.m
@@ -2,5 +2,28 @@
 
 tic;
 %% Your code here
+input1 = load('../data/lionCrop.mat');
+input1 = input1.imageOrig;
+output1 = myUnsharpMasking(input1,2,3);
+
+input2 = load('../data/superMoonCrop.mat');
+input2 = input2.imageOrig;
+output2 = myUnsharpMasking(input2,3,3);
+
+colormap(gray)
+%plotting image lion
+subplot(1,2,1),subimage(input1)
+colorbar
+subplot(1,2,2),subimage(output1)
+colorbar
+
+%press any button to see second image
+waitforbuttonpress 
+
+%plotting second moon
+subplot(1,2,1),subimage(input2)
+colorbar
+subplot(1,2,2),subimage(output2)
+colorbar
 
 toc;
