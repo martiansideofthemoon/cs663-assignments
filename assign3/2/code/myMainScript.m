3b6f7b89ffb3c15c9f1e76af56d804759d0eca13
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2017-08-30 21:06:46 +0530

    Changing toc order

--- assign3/2/code/myMainScript.m
+++ assign3/2/code/myMainScript.m
@@ -8,8 +8,8 @@ input = imgaussfilt(input, 1);
 % Subsampling by factor of 2
 input = input(1:4:end , 1:4:end, :);
 output = myMeanShiftSegmentation(input, 2, 2, 10);
+toc
 imshow(input, 'InitialMagnification', 'fit')
 axis on, colorbar, waitforbuttonpress
 imshow(output, 'InitialMagnification', 'fit')
 axis on, colorbar, waitforbuttonpress
-toc;
