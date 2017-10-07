ae0993de31747add77453288b4969c0106c52069
sabSAThai <pranavsankhe40@gmail.com>
2017-10-07 13:59:20 +0530

    PCA using Covariance

---
+++ assign4/1/code/pca_covariance.m
@@ -0,0 +1,23 @@
+function [signals,PC,V] = pca_covariance(data)
+
+	
+	[M,N] = size(data);             %    data - MxN matrix of input data   [M dimensions, N trials]                     
+	
+	% subtract off the mean for each dimension
+	mn =  mean(data,2);
+	data = data - repmat(mn,1,N);
+	
+	% calculate the covariance matrix
+	covariance = 1 / (N-1) * data * data’;
+
+
+	%PC - each column is a PC and V - Mx1 matrix of variances
+	[PC, V] = eig(covariance);
+	% extract diagonal of matrix as vector
+	V = diag(V);
+	% sort the variances in decreasing order
+	[junk, rindices] = sort(-1*V);
+	V  = V(rindices);
+	PC = PC(:,rindices);
+	% project the original data set
+	signals = PC’ * data;                         signals - MxN matrix of projected data
