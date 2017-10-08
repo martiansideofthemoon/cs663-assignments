7a44e5d5830f38bdf8a4e87b711f8ae76863b31c
sabSAThai <pranavsankhe40@gmail.com>
2017-10-08 06:30:15 +0530

    Question 1 with covariance done

--- assign4/1/code/pca_covariance.m
+++ assign4/1/code/pca_covariance.m
@@ -1,26 +1,34 @@
-function [signals,PC,V] = pca_covariance(data)
+function [eigenfaces,signals,A,m] = pca_covariance(X,k)
 
+	m = mean(X,2); %% Computing the average face image m = (1/P)*sum(Xj's')  (j = 1 : P)
+	imgcount = size(X,2);
 	
-	[M,N] = size(data);             %    data - MxN matrix of input data   [M dimensions, N trials]                     
-	
-	% subtract off the mean for each dimension
-	mn =  mean(data,2);
-	data = data - repmat(mn,1,N);
-	
-	% calculate the covariance matrix
-	L = 1 / (N-1) * data’ * data  ;
+	A = [];
+	for i=1 : imgcount
+	    temp = double(X(:,i)) - m;
+	    A = [A temp];
+	end
+
+	L= A' * A;
+	[V,D] = eig(L);  %% V : eigenvector matrix  D : eigenvalue matrix
+
+	D = diag(D);   %extract the diagonal vector
+	[B, rindices] = sort(-1*D);
+	rindices = rindices(1:k);
+
+	L_eig_vec = [];
+
+	for i = rindices 
+	    L_eig_vec = [L_eig_vec V(:,i)];
+	end
+
+	eigenfaces = A * L_eig_vec;
+    signals = [];
+	for i = 1 : size(eigenfaces,2)
+       
+	    temp = eigenfaces' * A(:,i);
+	    signals = [signals temp];
+	end
+end
 
 
-	%PC - each column is a PC and V - Mx1 matrix of variances
-	[PC, V] = eig(L);
-	% extract diagonal of matrix as vector
-	PC = data*PC; 
-	
-	V = diag(V);
-	
-	% sort the variances in decreasing order
-	[junk, rindices] = sort(-1*V);
-	V  = V(rindices);
-	PC = PC(:,rindices);
-	% project the original data set
-	signals = PC’ * data;                         signals - MxN matrix of projected data
