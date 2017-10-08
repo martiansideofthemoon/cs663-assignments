366864b942be89ddb54d6772c92ae9d6cd091033
sabSAThai <pranavsankhe40@gmail.com>
2017-10-09 01:12:41 +0530

    question 4 main script

---
+++ assign4/4/code/MyMainScript.m
@@ -0,0 +1,21 @@
+-%% MyMainScript
+-
+-tic;
+-m = 5;
+-n = 7;
+-A = reshape(randperm(m * n), m, n);
+-% Generating SVD
+-[U, V, S] = mySVD(A);
+-
+-% Converting the vector into m*n form
+-diag_S = diag(S);
+-if n > m
+-    diag_S = cat(2, diag_S, zeros(m, n-m));
+-elseif m > n
+-    diag_S = diag_S(:, 1:n);
+-end
+-% Comparing values of A with U*S*V^T
+-A
+-U * diag_S * V'
+-toc
+-
