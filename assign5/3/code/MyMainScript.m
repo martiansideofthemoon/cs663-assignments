3152ea53a0fe0a072bea83f3977ab3b3531a149b
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2017-10-21 00:45:12 +0530

    Q3 done

---
+++ assign5/3/code/MyMainScript.m
@@ -0,0 +1,37 @@
+
+%% Load Image
+load('../data/image_low_frequency_noise.mat');
+fim = fftshift(fft2(Z));
+absfim = log(abs(fim) + 1);
+figure
+subplot(1,2,1)
+imshow(Z)
+axis on, colorbar
+subplot(1,2,2)
+imshow(absfim, [1, 16])
+axis on, colorbar, waitforbuttonpress
+%% Notch Filtering
+% The fourier transform indicates high frequency component at (119,124) and (139,134)
+% This was determined using find(13 < absfim & 14 > absfim)
+% We construct 5*5 notch filters at these locations to remove low frequency noise
+fim2 = fim;
+% filter size is (2*dim+1) * (2*dim+1)
+dim = 2;
+for i=-dim:dim
+    for j=-dim:dim
+        fim2(119 + i, 124 + j) = 0;
+        fim2(139 + i, 134 + j) = 0;
+    end
+end
+% Rescaling other points in image to preserve energy
+fim2 = fim2 * (256*256) / (256*256 - dim*dim);
+absfim2 = log(abs(fim2) + 1);
+img2 = ifft2(ifftshift(fim2));
+figure
+subplot(1,2,1)
+imshow(img2)
+axis on, colorbar
+subplot(1,2,2)
+imshow(absfim2, [1,16])
+axis on, colorbar, waitforbuttonpress
+
